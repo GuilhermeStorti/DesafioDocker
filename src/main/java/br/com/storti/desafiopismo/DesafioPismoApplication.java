1feb56b20de0eabb74ea2a9959f4ae84af6dda0a
Guilherme Storti <guilhermestorti@hotmail.com>
2023-05-12 14:23:08 -0300

    Adicionando primeiros arquivos e configuraćão de banco e docker-compose

---
+++ src/main/java/br/com/storti/desafiopismo/DesafioPismoApplication.java
@@ -0,0 +1,13 @@
+package br.com.storti.desafiopismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPismoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioPismoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/storti/desafiopismo/DesafioPismoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.storti.desafiopismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPismoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
