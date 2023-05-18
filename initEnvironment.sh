9bcbb3179bc56d1774286d104999e9674f4bfcaf
Guilherme Storti <guilhermestorti@hotmail.com>
2023-05-18 14:49:24 -0300

    Ajustes no script de start

--- initEnvironment.sh
+++ initEnvironment.sh
@@ -5,4 +5,6 @@ docker-compose rm -y
 sudo rm -rf volume
 
 docker-compose build
-docker-compose up
+docker-compose up -d
+
+gnome-terminal --tab -e "docker-compose logs -f -t  async" --tab -e "docker-compose logs -f -t  api" --tab -e "docker-compose logs -f -t  localstack" --tab -e "docker-compose logs -f -t  postgres"
